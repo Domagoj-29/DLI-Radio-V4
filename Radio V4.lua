be06515a27759bb88cf5365bac19f85da84ba52a
Domagoj29 <domagojradulovic@yahoo.com>
2026-01-01 15:27:47 +0100

    Update Radio V4.lua
    Removed initializing variables as locals outside onTick(), they are now global instead.
    All local variables are now lowercase and globals are uppercase.
    Removed unused function (delay).

--- Radio V4.lua
+++ Radio V4.lua
@@ -2,48 +2,6 @@
 -- GitHub: https://github.com/Domagoj-29
 -- Workshop: https://steamcommunity.com/profiles/76561198935577915/myworkshopfiles/
 
--- Initializing variables and tables
-
-local uiR=0
-local uiG=0
-local uiB=0
-
-local numberChannel={}
-local boolChannel={}
-
-local PTTButton=false
-local ExternalPTT=false
-local muteToggle=false
-
-local FrequencySet=0
-
-local horizontalGap=0
-local verticalGap=0
-
-local maxDigits=6
-local fourDigitTableOffset=0
-
-local Increments={false,false,false,false,false,false}
-local Decrements={false,false,false,false,false,false}
-local Digits={0,0,0,0,0,0}
-
-local incrementCapacitor={}
-local decrementCapacitor={}
-local digitUpDown={}
-
-local frequencyModeCoordinatesX={0,0,0,0,0,0}
-
-local numberDataScrollX=0
-local boolDataScrollX=0
-
-local signalStrength=0
-
-local DataButton=false
-local MuteButton=false
-
-local w=0
-local h=0
-
 -- onDraw functions
 
 local function drawFrequencyArrow(x,y,isRotated)
@@ -60,23 +18,23 @@ local function drawColon(x,y)
 	screen.drawRectF(x,y+3,1,1)
 end
 local function drawSignalStrengthBackground()
-	screen.drawRectF(w-7-horizontalGap*4,0,1+horizontalGap,1)
-	screen.drawRectF(w-5-horizontalGap*3,0,1+horizontalGap,2)
-	screen.drawRectF(w-3-horizontalGap*2,0,1+horizontalGap,3)
-	screen.drawRectF(w-1-horizontalGap,0,1+horizontalGap,4)
+	screen.drawRectF(w-7-HorizontalGap*4,0,1+HorizontalGap,1)
+	screen.drawRectF(w-5-HorizontalGap*3,0,1+HorizontalGap,2)
+	screen.drawRectF(w-3-HorizontalGap*2,0,1+HorizontalGap,3)
+	screen.drawRectF(w-1-HorizontalGap,0,1+HorizontalGap,4)
 end
 local function drawSignalStrengthIndicator(signalStrength)
 	if signalStrength>0 then
-		screen.drawRectF(w-7-horizontalGap*4,0,1+horizontalGap,1)
+		screen.drawRectF(w-7-HorizontalGap*4,0,1+HorizontalGap,1)
 	end
 	if signalStrength>0.25 then
-		screen.drawRectF(w-5-horizontalGap*3,0,1+horizontalGap,2)
+		screen.drawRectF(w-5-HorizontalGap*3,0,1+HorizontalGap,2)
 	end
 	if signalStrength>0.5 then
-		screen.drawRectF(w-3-horizontalGap*2,0,1+horizontalGap,3)
+		screen.drawRectF(w-3-HorizontalGap*2,0,1+HorizontalGap,3)
 	end
 	if signalStrength>0.75 then
-		screen.drawRectF(w-1-horizontalGap,0,1+horizontalGap,4)
+		screen.drawRectF(w-1-HorizontalGap,0,1+HorizontalGap,4)
 	end
 end
 local function drawInvisibleRectangles()
@@ -85,14 +43,14 @@ local function drawInvisibleRectangles()
 end
 local function drawReturnArrow(shadingOffset)
 	screen.drawLine(1+shadingOffset,1,3+shadingOffset,-1)
-	screen.drawLine(0+shadingOffset,2,5+shadingOffset+horizontalGap,2)
+	screen.drawLine(0+shadingOffset,2,5+shadingOffset+HorizontalGap,2)
 	screen.drawLine(1+shadingOffset,3,3+shadingOffset,5)
 end
 local function getHighlightColor(isSelected)
 	if isSelected then
 		return 255,127,0
 	else
-		return uiR,uiG,uiB
+		return UiR,UiG,UiB
 	end
 end
 local function getSignalColor(signalStrength)
@@ -144,16 +102,6 @@ local function createCapacitor()
 		return (dischargeCounter>0 and dischargeCounter<dischargeTicks) or (chargeCounter==chargeTicks and boolValue)
 	end
 end
---[[local function createDelay()
-	local counter=0
-	return function(boolValue,delayTicks)
-		counter=boolValue and counter+1 or delayTicks
-		if counter==delayTicks or counter==1 then
-			counter=0
-			return true
-		end
-	end
-end]]
 local function createScrollUpDown()
 	local counter=0
 	return function(down,up)
@@ -225,10 +173,10 @@ local function truncate(number)
 	end
 end
 local function dynamicDecimalRounding(number)
-	local clampedNumber=clamp(number,-10^(4+horizontalGap)+1,10^(5+horizontalGap)-1)
+	local clampedNumber=clamp(number,-10^(4+HorizontalGap)+1,10^(5+HorizontalGap)-1)
 	local truncatedNumber=truncate(clampedNumber)
 	local numberLength=string.len(tostring(truncatedNumber))
-	local decimals=math.max(0,4+math.floor(horizontalGap+0.5)-numberLength)
+	local decimals=math.max(0,4+math.floor(HorizontalGap+0.5)-numberLength)
 
 	local roundedNumber=string.format("%." .. decimals .. "f",clampedNumber)
 	return roundedNumber:gsub("(%..-)0+$", "%1"):gsub("%.$", "")
@@ -241,176 +189,190 @@ local function boolToString(boolValue)
 	end
 end
 
-for i=1,maxDigits do
+digitUpDown={}
+incrementCapacitor={}
+decrementCapacitor={}
+for i=1,6 do
 	digitUpDown[i]=createDigitUpDown()
 	incrementCapacitor[i]=createCapacitor()
 	decrementCapacitor[i]=createCapacitor()
 end
 
-numberDataScroll=createScrollUpDown()
-boolDataScroll=createScrollUpDown()
-previousDataMode=createStringMemoryGate() -- This remembers the last data (bool,number,video) mode you were in
-mutePushToToggle=createPushToToggle()
-returnButtonPulse=createPulse()
-cycleDataModesPulse=createPulse()
+local numberDataScroll=createScrollUpDown()
+local boolDataScroll=createScrollUpDown()
+local previousDataMode=createStringMemoryGate() -- This remembers the last data (bool,number,video) mode you were in
+local mutePushToToggle=createPushToToggle()
+local returnButtonPulse=createPulse()
+local cycleDataModesPulse=createPulse()
 
-local screenMode="Menu" -- "Menu","Frequency","NumberData","BoolData","VideoData"
+w=0
+h=0
+HorizontalGap=0
+VerticalGap=0
+FrequencySet=0
+NumberDataScrollY=0
+BoolDataScrollY=0
+ScreenMode="Menu" -- "Menu","Frequency","NumberData","BoolData","VideoData"
+NumberChannel={}
+BoolChannel={}
+Increments={}
+Decrements={}
+Digits={0,0,0,0,0,0}
+
+UiR=property.getNumber("UI R")
+UiG=property.getNumber("UI G")
+UiB=property.getNumber("UI B")
 function onTick()
-	uiR=property.getNumber("UI R")
-	uiG=property.getNumber("UI G")
-	uiB=property.getNumber("UI B")
-
 	local inputX=input.getNumber(3)
 	local inputY=input.getNumber(4)
-	signalStrength=input.getNumber(7)
+	SignalStrength=input.getNumber(7)
 
 	for i=1,8 do
-		numberChannel[i]=dynamicDecimalRounding(input.getNumber(7+i))
+		NumberChannel[i]=dynamicDecimalRounding(input.getNumber(7+i))
 	end
 
     local isPressed=input.getBool(1)
 	ExternalPTT=input.getBool(3)
 
 	for i=1,8 do
-		boolChannel[i]=boolToString(input.getBool(3+i))
+		BoolChannel[i]=boolToString(input.getBool(3+i))
 	end
 
-	horizontalGap=clamp((w/32-1),0,2)
-	verticalGap=clamp((h/32-1),0,2)
+	HorizontalGap=clamp((w/32-1),0,2)
+	VerticalGap=clamp((h/32-1),0,2)
 
-	local ReturnButton=isPressed and isPointInRectangle(inputX,inputY,-1,-1,6+horizontalGap,6)
+	local ReturnButton=isPressed and isPointInRectangle(inputX,inputY,-1,-1,6+HorizontalGap,6)
 	if returnButtonPulse(ReturnButton) then
-		previousDataMode(screenMode,screenMode~="Frequency")
-		screenMode="Menu"
+		previousDataMode(ScreenMode,ScreenMode~="Frequency")
+		ScreenMode="Menu"
 	end
 
-	maxDigits=w==32 and 4 or 6
-	fourDigitTableOffset=w==32 and 1 or 0
-	frequencyModeCoordinatesX={w/2+10,w/2+5,w/2,w/2-6,w/2-11,w/2-16}
+	MaxDigits=w==32 and 4 or 6
+	FourDigitTableOffset=w==32 and 1 or 0
+	FrequencyModeCoordinatesX={w/2+10,w/2+5,w/2,w/2-6,w/2-11,w/2-16}
 
 	local cycleDataModes=isPressed and isPointInRectangle(inputX,inputY,w/2-9,-1,16,6)
 
-	local Up=isPressed and isPointInRectangle(inputX,inputY,-1,-1,w+2,h/2-2)
-	local Down=isPressed and isPointInRectangle(inputX,inputY,-1,h/2+1,w+2,h/2)
+	local up=isPressed and isPointInRectangle(inputX,inputY,-1,-1,w+2,h/2-2)
+	local down=isPressed and isPointInRectangle(inputX,inputY,-1,h/2+1,w+2,h/2)
 
-	DataButton=false
+	local dataButton=false
 	local videoSwitchbox=false
 
-	if screenMode=="Menu" then
-		local FrqButton=isPressed and isPointInRectangle(inputX,inputY,w/2-8,h/2-16,15,6)
-		PTTButton=isPressed and isPointInRectangle(inputX,inputY,w/2-8,h/2-10+verticalGap,15,6)
-		DataButton=isPressed and isPointInRectangle(inputX,inputY,w/2-11,h/2-4+verticalGap*2,20,6)
-		MuteButton=isPressed and isPointInRectangle(inputX,inputY,w/2-11,h/2+2+verticalGap*3,20,6)
+	if ScreenMode=="Menu" then
+		local frqButton=isPressed and isPointInRectangle(inputX,inputY,w/2-8,h/2-16,15,6)
+		PTTButton=isPressed and isPointInRectangle(inputX,inputY,w/2-8,h/2-10+VerticalGap,15,6)
+		dataButton=isPressed and isPointInRectangle(inputX,inputY,w/2-11,h/2-4+VerticalGap*2,20,6)
+		local muteButton=isPressed and isPointInRectangle(inputX,inputY,w/2-11,h/2+2+VerticalGap*3,20,6)
 
-		if FrqButton then
-			screenMode="Frequency"
+		if frqButton then
+			ScreenMode="Frequency"
 		end
 		PTTButtonR,PTTButtonG,PTTButtonB=getHighlightColor(PTTButton or ExternalPTT)
-		if DataButton then
-			screenMode=previousDataMode(nil,false)
+		if dataButton then
+			ScreenMode=previousDataMode(nil,false)
 		end
-		muteToggle=mutePushToToggle(MuteButton)
-		muteButtonR,muteButtonG,muteButtonB=getHighlightColor(muteToggle)
-	elseif screenMode=="Frequency" then
-		for i=1,maxDigits do
-			Increments[i]=isPressed and isPointInRectangle(inputX,inputY,frequencyModeCoordinatesX[i+fourDigitTableOffset],h/2-9,5,6)
-			Decrements[i]=isPressed and isPointInRectangle(inputX,inputY,frequencyModeCoordinatesX[i+fourDigitTableOffset],h/2+1,5,6)
+		MuteToggle=mutePushToToggle(muteButton)
+	elseif ScreenMode=="Frequency" then
+		for i=1,MaxDigits do
+			Increments[i]=isPressed and isPointInRectangle(inputX,inputY,FrequencyModeCoordinatesX[i+FourDigitTableOffset],h/2-9,5,6)
+			Decrements[i]=isPressed and isPointInRectangle(inputX,inputY,FrequencyModeCoordinatesX[i+FourDigitTableOffset],h/2+1,5,6)
 			Digits[i]=digitUpDown[i](Increments[i],Decrements[i])
 		end
 		FrequencySet=Digits[1]+Digits[2]*10+Digits[3]*100+Digits[4]*1000+Digits[5]*10000+Digits[6]*100000
-	elseif screenMode=="NumberData" then
-		local notAnyButton=not (ReturnButton or DataButton or cycleDataModes)
-		numberDataScrollX=h==32 and numberDataScroll(Down and notAnyButton,Up and notAnyButton) or 0
+	elseif ScreenMode=="NumberData" then
+		local notAnyButton=not (ReturnButton or dataButton or cycleDataModes)
+		NumberDataScrollY=h==32 and numberDataScroll(down and notAnyButton,up and notAnyButton) or 0
 
 		if cycleDataModesPulse(cycleDataModes) then
-			screenMode="BoolData"
+			ScreenMode="BoolData"
 		end
-	elseif screenMode=="BoolData" then
-		local notAnyButton=not (ReturnButton or DataButton or cycleDataModes)
-		boolDataScrollX=h==32 and boolDataScroll(Down and notAnyButton,Up and notAnyButton) or 0
+	elseif ScreenMode=="BoolData" then
+		local notAnyButton=not (ReturnButton or dataButton or cycleDataModes)
+		BoolDataScrollY=h==32 and boolDataScroll(down and notAnyButton,up and notAnyButton) or 0
 
 		if cycleDataModesPulse(cycleDataModes) then
-			screenMode="VideoData"
+			ScreenMode="VideoData"
 		end
-	elseif screenMode=="VideoData" then
+	elseif ScreenMode=="VideoData" then
 		videoSwitchbox=true
 		if cycleDataModesPulse(cycleDataModes) then
-			screenMode="NumberData"
+			ScreenMode="NumberData"
 		end
 	end
 
 	output.setNumber(1,FrequencySet)
 
 	output.setBool(1,PTTButton or ExternalPTT)
-	output.setBool(2,muteToggle)
+	output.setBool(2,MuteToggle)
 	output.setBool(3,videoSwitchbox)
 end
 function onDraw()
 	w=screen.getWidth()
 	h=screen.getHeight()
 
-	if screenMode~="VideoData" then
+	if ScreenMode~="VideoData" then
 		screen.setColor(15,15,15)
 		screen.drawClear()
 	end
 
 	screen.setColor(0,0,0)
-	if screenMode=="Menu" then
+	if ScreenMode=="Menu" then
 		screen.drawText(w/2-6,h/2-15,"FRQ")
-		screen.drawText(w/2-6,h/2-9+verticalGap,"PTT")
-		screen.drawText(w/2-9,h/2-3+verticalGap*2,"DATA")
-		screen.drawText(w/2-9,h/2+3+verticalGap*3,"MUTE")
+		screen.drawText(w/2-6,h/2-9+VerticalGap,"PTT")
+		screen.drawText(w/2-9,h/2-3+VerticalGap*2,"DATA")
+		screen.drawText(w/2-9,h/2+3+VerticalGap*3,"MUTE")
 		screen.drawText(w-9,h-5,"V4")
-	elseif screenMode=="Frequency" then
-		for i=1,maxDigits do
-			screen.drawText(frequencyModeCoordinatesX[i+fourDigitTableOffset]+2,h/2-3,string.format("%.0f",Digits[i]))
-			drawFrequencyArrow(frequencyModeCoordinatesX[i+fourDigitTableOffset]+2,h/2-6,false)
-			drawFrequencyArrow(frequencyModeCoordinatesX[i+fourDigitTableOffset]+2,h/2+3,true)
+	elseif ScreenMode=="Frequency" then
+		for i=1,MaxDigits do
+			screen.drawText(FrequencyModeCoordinatesX[i+FourDigitTableOffset]+2,h/2-3,string.format("%.0f",Digits[i]))
+			drawFrequencyArrow(FrequencyModeCoordinatesX[i+FourDigitTableOffset]+2,h/2-6,false)
+			drawFrequencyArrow(FrequencyModeCoordinatesX[i+FourDigitTableOffset]+2,h/2+3,true)
 		end
-	elseif screenMode=="NumberData" then
+	elseif ScreenMode=="NumberData" then
 		for i=1,8 do
-			screen.drawText(1,i*6+i*verticalGap+numberDataScrollX,string.format("%.0f",i))
-			drawColon(6,i*6+i*verticalGap+numberDataScrollX)
-			screen.drawTextBox(w-35,i*6+i*verticalGap+numberDataScrollX,35,5,numberChannel[i],1)
+			screen.drawText(1,i*6+i*VerticalGap+NumberDataScrollY,string.format("%.0f",i))
+			drawColon(6,i*6+i*VerticalGap+NumberDataScrollY)
+			screen.drawTextBox(w-35,i*6+i*VerticalGap+NumberDataScrollY,35,5,NumberChannel[i],1)
 		end
 		screen.drawText(w/2-7,0,"Num")
-	elseif screenMode=="BoolData" then
+	elseif ScreenMode=="BoolData" then
 		for i=1,8 do
-			screen.drawText(1,i*6+i*verticalGap+boolDataScrollX,string.format("%.0f",i))
-			drawColon(6,i*6+i*verticalGap+boolDataScrollX)
-			screen.drawTextBox(w-15,i*6+i*verticalGap+boolDataScrollX,15,5,boolChannel[i],1)
+			screen.drawText(1,i*6+i*VerticalGap+BoolDataScrollY,string.format("%.0f",i))
+			drawColon(6,i*6+i*VerticalGap+BoolDataScrollY)
+			screen.drawTextBox(w-15,i*6+i*VerticalGap+BoolDataScrollY,15,5,BoolChannel[i],1)
 		end
-	elseif screenMode=="VideoData" then
+	elseif ScreenMode=="VideoData" then
 		screen.drawText(w/2-7,0,"Vid")
 	end
 
-	if screenMode=="Menu" then
-		screen.setColor(uiR,uiG,uiB)
+	if ScreenMode=="Menu" then
+		screen.setColor(UiR,UiG,UiB)
 		screen.drawText(w/2-7,h/2-15,"FRQ")
-		screen.drawText(w/2-10,h/2-3+verticalGap*2,"DATA")
+		screen.drawText(w/2-10,h/2-3+VerticalGap*2,"DATA")
 		screen.drawText(w-10,h-5,"V4")
 
 		screen.setColor(getHighlightColor(PTTButton or ExternalPTT))
-		screen.drawText(w/2-7,h/2-9+verticalGap,"PTT")
-		screen.setColor(getHighlightColor(muteToggle))
-		screen.drawText(w/2-10,h/2+3+verticalGap*3,"MUTE")
-	elseif screenMode=="Frequency" then
-		for i=1,maxDigits do
-			screen.setColor(uiR,uiG,uiB)
-			screen.drawText(frequencyModeCoordinatesX[i+fourDigitTableOffset]+1,h/2-3,string.format("%.0f",Digits[i]))
+		screen.drawText(w/2-7,h/2-9+VerticalGap,"PTT")
+		screen.setColor(getHighlightColor(MuteToggle))
+		screen.drawText(w/2-10,h/2+3+VerticalGap*3,"MUTE")
+	elseif ScreenMode=="Frequency" then
+		for i=1,MaxDigits do
+			screen.setColor(UiR,UiG,UiB)
+			screen.drawText(FrequencyModeCoordinatesX[i+FourDigitTableOffset]+1,h/2-3,string.format("%.0f",Digits[i]))
 
 			screen.setColor(getHighlightColor(incrementCapacitor[i](Increments[i],1,15)))
-			drawFrequencyArrow(frequencyModeCoordinatesX[i+fourDigitTableOffset]+1,h/2-6,false)
+			drawFrequencyArrow(FrequencyModeCoordinatesX[i+FourDigitTableOffset]+1,h/2-6,false)
 
 			screen.setColor(getHighlightColor(decrementCapacitor[i](Decrements[i],1,15)))
-			drawFrequencyArrow(frequencyModeCoordinatesX[i+fourDigitTableOffset]+1,h/2+3,true)
+			drawFrequencyArrow(FrequencyModeCoordinatesX[i+FourDigitTableOffset]+1,h/2+3,true)
 		end
-	elseif screenMode=="NumberData" then
-		screen.setColor(uiR,uiG,uiB)
+	elseif ScreenMode=="NumberData" then
+		screen.setColor(UiR,UiG,UiB)
 		for i=1,8 do
-			screen.drawText(0,i*6+i*verticalGap+numberDataScrollX,string.format("%.0f",i))
-			drawColon(5,i*6+i*verticalGap+numberDataScrollX)
-			screen.drawTextBox(w-36,i*6+i*verticalGap+numberDataScrollX,35,5,numberChannel[i],1)
+			screen.drawText(0,i*6+i*VerticalGap+NumberDataScrollY,string.format("%.0f",i))
+			drawColon(5,i*6+i*VerticalGap+NumberDataScrollY)
+			screen.drawTextBox(w-36,i*6+i*VerticalGap+NumberDataScrollY,35,5,NumberChannel[i],1)
 		end
 
 		screen.setColor(15,15,15)
@@ -419,14 +381,14 @@ function onDraw()
 		screen.setColor(0,0,0)
 		screen.drawText(w/2-7,0,"Num")
 
-		screen.setColor(uiR,uiG,uiB)
+		screen.setColor(UiR,UiG,UiB)
 		screen.drawText(w/2-8,0,"Num")
-	elseif screenMode=="BoolData" then
-		screen.setColor(uiR,uiG,uiB)
+	elseif ScreenMode=="BoolData" then
+		screen.setColor(UiR,UiG,UiB)
 		for i=1,8 do
-			screen.drawText(0,i*6+i*verticalGap+boolDataScrollX,string.format("%.0f",i))
-			drawColon(5,i*6+i*verticalGap+boolDataScrollX)
-			screen.drawTextBox(w-16,i*6+i*verticalGap+boolDataScrollX,15,5,boolChannel[i],1)
+			screen.drawText(0,i*6+i*VerticalGap+BoolDataScrollY,string.format("%.0f",i))
+			drawColon(5,i*6+i*VerticalGap+BoolDataScrollY)
+			screen.drawTextBox(w-16,i*6+i*VerticalGap+BoolDataScrollY,15,5,BoolChannel[i],1)
 		end
 
 		screen.setColor(15,15,15)
@@ -435,24 +397,24 @@ function onDraw()
 		screen.setColor(0,0,0)
 		screen.drawText(w/2-7,0,"Log")
 
-		screen.setColor(uiR,uiG,uiB)
+		screen.setColor(UiR,UiG,UiB)
 		screen.drawText(w/2-8,0,"Log")
-	elseif screenMode=="VideoData" then
-		screen.setColor(uiR,uiG,uiB)
+	elseif ScreenMode=="VideoData" then
+		screen.setColor(UiR,UiG,UiB)
 		screen.drawText(w/2-8,0,"Vid")
 	end
 
-	if screenMode~="Menu" then
+	if ScreenMode~="Menu" then
 		screen.setColor(0,0,0)
 		drawReturnArrow(1)
-		screen.setColor(uiR,uiG,uiB)
+		screen.setColor(UiR,UiG,UiB)
 		drawReturnArrow(0)
 	end
 
-	if screenMode~="VideoData" then
+	if ScreenMode~="VideoData" then
 		screen.setColor(25,25,25)
 		drawSignalStrengthBackground()
 	end
-	screen.setColor(getSignalColor(signalStrength))
-	drawSignalStrengthIndicator(signalStrength)
+	screen.setColor(getSignalColor(SignalStrength))
+	drawSignalStrengthIndicator(SignalStrength)
 end
